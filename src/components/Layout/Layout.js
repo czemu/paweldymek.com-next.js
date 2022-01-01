3b2a6ebf2f299046d0de42ce3a2a5801da844b70
Paweł Dymek <pawel.dymek@gmail.com>
2022-01-01 21:51:37 +0100

    Add GA code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -36,6 +36,21 @@ export default function Layout({ children }) {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&amp;display=swap" rel="stylesheet" />
                 <link rel="canonical" href={APP_URL + '/' + state.locale + router.asPath} />
+
+                <script
+                    dangerouslySetInnerHTML={{
+                    __html:
+                        `
+                            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                
+                            ga('create', 'UA-836606-11', 'auto');
+                            ga('send', 'pageview');
+                        `
+                    }}
+                />
             </Head>
 
             <div id="wrapper">
